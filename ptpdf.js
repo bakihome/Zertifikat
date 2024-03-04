30854807b232768235064d309ad2d909c379f2ae
bakihome <156809280+bakihome@users.noreply.github.com>
2024-03-04 20:26:20 +0100

    print update
    print button is working on screen

---
+++ ptpdf.js
@@ -0,0 +1,14 @@
+
+let name;
+
+function insertName(){
+name = document.querySelector(':root');
+name = prompt("Geben sie bitte Ihren Namen ein: ");
+}
+function doc(){
+    document.write(name);}
+
+
+
+
+
